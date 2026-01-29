ec73af600c225e202ed8818253b09db4c3f31c33
MagicMenma <1479800445@qq.com>
2026-01-29 05:03:14 -0500

    0.11
    SelectableObject

--- Levels/zoo.gd
+++ Levels/zoo.gd
@@ -3,9 +3,13 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	print("Start")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _input(event):
+	if event is InputEventMouseButton and event.pressed:
+		print("屏幕被点击了，点击位置: ", event.position)
